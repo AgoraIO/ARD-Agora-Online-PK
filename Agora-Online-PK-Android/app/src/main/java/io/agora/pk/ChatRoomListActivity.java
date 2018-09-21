3f890ef75342cfcfedfbe40de1c3150af121624d
wangdaoxin <wdx700@gmail.com>
2018-09-21 15:33:11 +0800

    remove fake room list related code

--- Agora-Online-PK-Android/app/src/main/java/io/agora/pk/ChatRoomListActivity.java
+++ Agora-Online-PK-Android/app/src/main/java/io/agora/pk/ChatRoomListActivity.java
@@ -2,7 +2,6 @@ package io.agora.pk;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.ColorDrawable;
@@ -12,45 +11,27 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.lang.ref.WeakReference;
-import java.util.ArrayList;
-import java.util.List;
-
-import io.agora.pk.bean.CRLBean;
-import io.agora.pk.ui.CRLItemDecor;
-import io.agora.pk.ui.CRLRecycleAdapter;
-import io.agora.pk.utils.ChatRoomListCreator;
 import io.agora.pk.utils.PKConstants;
 import io.agora.pk.utils.StringUtils;
 import io.agora.rtc.Constants;
 
-public class ChatRoomListActivity extends Activity implements ChatRoomListCreator.CreatorListener {
-    private RecyclerView mRvChatRoomList;
-    private CRLRecycleAdapter mRVAdapter;
+public class ChatRoomListActivity extends Activity {
 
     private boolean hasPermission = false;
-
-    private ChatRoomListCreator crlc;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room_list);
 
-        crlc = new ChatRoomListCreator(new WeakReference<Context>(this), this);
         if (checkSelfPermissions()) {
             hasPermission = true;
-            crlc.execute();
         }
     }
 
@@ -94,23 +75,6 @@ public class ChatRoomListActivity extends Activity implements ChatRoomListCreato
         }
     }
 
-    @Override
-    public void creatFinished(List<CRLBean> beans) {
-        mRvChatRoomList = findViewById(R.id.rv_chat_room_list_recycler_view);
-        Animation animation = AnimationUtils.loadAnimation(this, R.anim.rv_view_load);
-        mRvChatRoomList.setAnimation(animation);
-        mRVAdapter = new CRLRecycleAdapter(this, beans);
-        mRvChatRoomList.setFocusable(true);
-        mRvChatRoomList.setHasFixedSize(true);
-
-        mRvChatRoomList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        mRvChatRoomList.setAdapter(mRVAdapter);
-        mRvChatRoomList.setDrawingCacheEnabled(true);
-        mRvChatRoomList.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_AUTO);
-
-        mRvChatRoomList.addItemDecoration(new CRLItemDecor());
-    }
-
     private boolean checkSelfPermissions() {
         return checkSelfPermission(Manifest.permission.RECORD_AUDIO, 200) &&
                 checkSelfPermission(Manifest.permission.CAMERA, 201) &&
@@ -146,7 +110,6 @@ public class ChatRoomListActivity extends Activity implements ChatRoomListCreato
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, 202);
                     hasPermission = true;
-                    crlc.execute();
                 } else {
                     finish();
                 }
--- Agora-Online-PK-Android/app/src/main/java/io/agora/pk/bean/CRLBean.java
+++
@@ -1,9 +0,0 @@
-package io.agora.pk.bean;
-
-import android.graphics.Bitmap;
-
-public class CRLBean {
-    public int userIconId;
-    public Bitmap broadcasterIconId;
-    public String channelAccount;
-}
